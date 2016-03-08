55aea6a53002d52fec13ce07a9a5227cd85cf8e3
Alexander Fuchs <alexander.fsoares@outlook.com>
2016-03-07 22:03:26 -0300

    inlusão de estilo na pagina inicial do projeto.

--- estacionamento/index.php
+++ estacionamento/index.php
@@ -3,8 +3,11 @@
 <head>
 	<meta charset="UTF-8" />
 	<title>Pagina inicial do projeto</title>
+	<link rel="stylesheet" href="bootstrap/min.css"/>
 </head>
 <body>
 	<h1>página inicial do projeto</h1>
+	<?php print "Impresso em php";
+	?>
 </body>
 </html>
