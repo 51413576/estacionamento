e58670a75baa91582edbb602c7437f8a57d4a76b
Alexander Fuchs <alexander.fsoares@outlook.com>
2016-03-07 21:54:41 -0300

    estruturação da pagina inicial do projeto.

--- estacionamento/index.php
+++ estacionamento/index.php
@@ -1,2 +1,10 @@
-<?php
-print "pagina incial do projeto";
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8" />
+	<title>Pagina inicial do projeto</title>
+</head>
+<body>
+	<h1>página inicial do projeto</h1>
+</body>
+</html>
